a6eb2246af9b2831095e961cac6b7f1c00c3660b
Zulfaabam <zulfafatahakbar@gmail.com>
2026-01-12 21:17:27 +0700

    wip: pokemon list & detail

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,75 +1,62 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { getPokemonList } from '@/services/pokeApi'
+import { PokemonList } from '@/types/pokemon'
+import { Image } from 'expo-image'
+import { useEffect, useState } from 'react'
+import { FlatList, Pressable, StyleSheet, Text } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function Home() {
+  const [pokemon, setPokemon] = useState<PokemonList>({
+    count: 0,
+    next: '',
+    previous: '',
+    results: [],
+  })
 
-export function Home() {
+  useEffect(() => {
+    getPokemonList(50, 0)
+      .then((res) => {
+        console.log(res.data)
+        setPokemon(res.data)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">src/navigation/screens/Home.tsx</ThemedText> to
-          see changes. Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <FlatList
+      data={pokemon.results}
+      keyExtractor={(item) => item?.name}
+      style={styles.list}
+      renderItem={({ item }) => {
+        const number = item?.url
+          .split('/')
+          [item?.url.split('/').length - 2].padStart(4, '0')
+        return (
+          <Pressable
+            // onPress={() =>
+            //   navigation.navigate('PokemonDetail', { name: item.name })
+            // }
+            style={styles.item}
+          >
+            <Text style={styles.text}>{number}</Text>
+            <Text style={styles.text}>{item?.name}</Text>
+          </Pressable>
+        )
+      }}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  list: {},
+  item: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 16,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text: {
+    textTransform: 'capitalize',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+})
